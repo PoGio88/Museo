1f1f66f3ab4884bf48040d20dd4e0bcebf3611ac
danielepica <73017443+danielepica@users.noreply.github.com>
2021-06-26 14:06:17 +0200

    Inizio VisualizzaOpera

--- museo/src/main/java/it/uniroma3/siw/museo/MuseoApplication.java
+++ museo/src/main/java/it/uniroma3/siw/museo/MuseoApplication.java
@@ -1,14 +1,8 @@
 package it.uniroma3.siw.museo;
 
 
-import it.uniroma3.siw.museo.model.Credentials;
-import it.uniroma3.siw.museo.repository.CredentialsRepository;
-import javafx.scene.canvas.GraphicsContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.Console;
 
 @SpringBootApplication
 public class MuseoApplication {
--- museo/src/main/java/it/uniroma3/siw/museo/controller/MainController.java
+++ museo/src/main/java/it/uniroma3/siw/museo/controller/MainController.java
@@ -1,13 +1,26 @@
 package it.uniroma3.siw.museo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.museo.service.MuseoService;
+
 @Controller
 public class MainController {
 	
+	@Autowired
+    private MuseoService service;
+	
+	@RequestMapping(value = "/visualizzaOpere", method = RequestMethod.GET)
+	public String visualizzaOpere(Model model) { 
+		model.addAttribute("opere",service.tutteLeOpere());
+		return "opere.html" ; }
+	
 	@RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
 	public String index(Model model) { return "index.html";	}
+	
+	
 }
